0128b1c8a39142162a171879f14dae2c5d0d9a8a
andrew nefsky <anefsky@gmail.com>
2022-11-21 11:03:42 -0500

    initial page layout

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import{ Header, Main }  from './styles.js';
+
+function App() {
+  return (
+    <>
+      <Header>
+        React-hooks-grid header
+      </Header>
+      <Main>
+        In content div
+      </Main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/App/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    height: 30vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Main = styled.div`
+    height: 70vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
